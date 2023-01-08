470249db30d3e2f703df4fa238c48b3bee843b2d
İlhan Sarı <ilhansari94@hotmail.com>
2023-01-08 11:44:38 +0100

    initial commit

---
+++ TravelFun/TravelFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelFunApp.swift
+//  TravelFun
+//
+//  Created by Ilhan Sari on 08/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct TravelFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
