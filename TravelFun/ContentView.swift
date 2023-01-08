49ea2e17b5402614892aec4147e7d035748fe9d1
İlhan Sarı <ilhansari94@hotmail.com>
2023-01-08 12:06:01 +0100

    Create folders and files structure

--- TravelFun/ContentView.swift
+++ TravelFun/ContentView.swift
@@ -8,19 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+
+  init() {
+    UINavigationBar.appearance().largeTitleTextAttributes = [
+      .foregroundColor: UIColor.white
+    ]
+  }
+
+  var body: some View {
+    NavigationView {
+      HomeView()
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
---
+++ TravelFun/Presentation/Home/HomeView.swift
@@ -0,0 +1,69 @@
+//
+//  HomeView.swift
+//  TravelFun
+//
+//  Created by Ilhan Sari on 08/01/2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+  
+  var body: some View {
+    ZStack {
+
+      LinearGradient(colors: [Color(#colorLiteral(red: 1, green: 0.6967965364, blue: 0.2615769506, alpha: 1)), Color(#colorLiteral(red: 1, green: 0.5921096206, blue: 0.2459464371, alpha: 1))],
+                     startPoint: .center,
+                     endPoint: .bottom)
+      .ignoresSafeArea()
+
+      Color(.init(white: 0.95, alpha: 1))
+        .offset(y: 400)
+
+      ScrollView {
+
+        TextFieldView()
+
+        DiscoverCategoriesView()
+
+        VStack {
+          PopularDestinationsView()
+
+          PopularPlacesView()
+
+          TrendingCreatorsView()
+        }
+        .background(Color(.init(white: 0.95, alpha: 1)))
+        .cornerRadius(16)
+        .padding(.top, 32)
+
+      }
+    }
+    .navigationTitle("Discover")
+  }
+}
+
+struct HomeView_Previews: PreviewProvider {
+  static var previews: some View {
+    HomeView()
+  }
+}
+
+struct TextFieldView: View {
+
+  var body: some View {
+    HStack() {
+      Image(systemName: "magnifyingglass")
+        .foregroundColor(Color.white)
+      Text("Where do you want go?")
+      Spacer()
+    }
+    .font(.system(size: 14, weight: .semibold))
+    .foregroundColor(.white)
+    .padding()
+    .background(Color(.init(white: 0.95, alpha: 0.3)))
+    .cornerRadius(12)
+    .padding(16)
+
+  }
+}
